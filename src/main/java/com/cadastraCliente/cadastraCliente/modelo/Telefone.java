ed49bac40059bee0b0eb1b2fd34fae5cc05dd982
Tiago Soares <tiago.soares@cooperforte.coop.br>
2019-11-25 17:15:32 -0300

    ATUALIZACAO

---
+++ src/main/java/com/cadastraCliente/cadastraCliente/modelo/Telefone.java
@@ -0,0 +1,48 @@
+package com.cadastraCliente.cadastraCliente.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Telefone {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String tipo;
+    private int numero;
+    Cliente cliente;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+}
