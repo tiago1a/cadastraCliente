0ee2d99416aed7e88e6fce4e3df47d0666b0b68a
tiago1a <tiagoamaro01@gmail.com>
2019-11-25 23:36:03 -0300

    atualizacao

--- src/main/java/com/cadastraCliente/cadastraCliente/modelo/Telefone.java
+++ src/main/java/com/cadastraCliente/cadastraCliente/modelo/Telefone.java
@@ -5,7 +5,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity
 public class Telefone {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
